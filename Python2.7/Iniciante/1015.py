fb0934db5a4a25a482e74be1c43cdfe60c3f1fd1
Vinicius <viniciuslcp97@gmail.com>
2016-09-20 13:07:19 -0300

    exercício 1015 resolvido

--- Python2.7/Iniciante/1015.py
+++ Python2.7/Iniciante/1015.py
@@ -2,9 +2,9 @@
 #-*- coding: utf-8 -*-
 import math
 
-X = raw_input()
-Y = raw_input()
-X = X.split(' ')
-Y = Y.split(' ')
-distancia = math.sqrt(((float(X[1]) - float(X[0])) ** 2) + ((float(Y[1]) - float(Y[0])) ** 2))
+p1 = raw_input()
+p2 = raw_input()
+p1 = p1.split(' ')
+p2 = p2.split(' ')
+distancia = math.sqrt(((float(p2[0]) - float(p1[0])) ** 2) + ((float(p2[1]) - float(p1[1])) ** 2))
 print "{:0.4f}".format(distancia)
